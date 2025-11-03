b14ba9f9ec42a5332493ce01fe112664df20afa0
ejonghwan <sun87-1@daum.net>
2025-11-03 23:56:42 +0900

    a

---
+++ src/hooks/target.ts
@@ -0,0 +1,22 @@
+"use client"
+
+import React, { useRef, useState } from 'react'
+
+
+interface Props {
+   el: any;
+}
+
+const Target = ({ el }: Props) => {
+
+
+   const tRef = useRef(el)
+
+   // tRef.current
+
+
+
+   return
+}
+
+export default Target
